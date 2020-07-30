229afeb039a4423152a0722e97d7dc9e87d42806
Dubovaya <dubovaya.java@gmail.com>
2020-07-30 16:57:16 +0300

    fx style

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 public class Palindrome {
-
     public boolean isPalindrome(String text) {
         text = text.toLowerCase().replaceAll("[^a-z0-9]","");
         StringBuilder result = new StringBuilder(text);
